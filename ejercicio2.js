62eef5824032ce928bdab050366bfff4377e8a7c
Jose Alejandro Ferrera Chacon <jaferrerac@unah.hn>
2026-06-12 01:23:08 -0600

    Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,21 @@
+function consultarBaseDeDatos(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            const random =Math.floor(Math.random()*10);
+            console.log(random)
+            if(random % 2 === 0){
+                resolve(true)
+            }
+            reject(`Error en la consulta ${random}`)
+        }, 3000); 
+    })
+    
+}
+
+consultarBaseDeDatos()
+    .then((resultado)=>{
+        console.log('Consulta exitosa')
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
